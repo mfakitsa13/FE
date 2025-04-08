6ec5e67923d8298e7514866e6da6e71fc7c23dfb
mfakitsa13 <131189164+mfakitsa13@users.noreply.github.com>
2025-04-08 16:32:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const container = document.getElementById('root');
+const root = createRoot(container); 
+root.render(<App />);
 root.render(
   <React.StrictMode>
     <App />
